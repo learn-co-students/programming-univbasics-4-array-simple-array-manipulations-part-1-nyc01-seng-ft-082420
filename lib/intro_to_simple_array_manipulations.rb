2dad6ee5ff079be599e23a2885797c75c7c3cd7f
Bryam Vicente <vicentebryam@gmail.com>
2020-08-10 03:37:16 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,28 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+# return array
+end
+
+def pop_with_args(array)
+  array.pop(2)
+
+end
+
+def using_shift(array)
+  array.shift
+# return array[0]
+end
+
+def shift_with_args(array)
+  array.shift(2)
+
+
+end
