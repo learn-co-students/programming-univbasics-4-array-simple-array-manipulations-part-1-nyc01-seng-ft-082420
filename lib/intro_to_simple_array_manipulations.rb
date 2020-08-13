b1676a7bf0a4c11307be8f7632885eb70e051f81
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-13 01:18:57 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,113 @@
+
+
+# [V]
+
+def 
+  
+  using_push(array, string)
+  
+  
+  array.push(string)
+  
+end
+
+
+# using_push
+#   takes in two arguments, an array and a string and adds that string to the endof the array using the push method (FAILED - 1)
+
+
+
+# [V]
+
+def 
+  
+  using_unshift(array, string)
+  
+  
+  array.unshift(string)
+  
+end
+
+# using_unshift
+#   takes in two arguments, an array and a string and adds that string to the front of the array using the unshift method (FAILED - 2)
+#   increases the length of the array (FAILED - 3)
+
+
+
+
+# [V]
+
+def 
+  
+  using_pop(array)
+  
+
+  array.pop()
+  
+end
+
+# using_pop
+#   takes in argument of an array and uses the pop method to remove the last element from the array and return that element (FAILED - 4)
+#   decreases the length of the array by 1 (FAILED - 5)
+
+
+
+
+#[V]
+
+def 
+  
+  pop_with_args(array)
+  # {...}(pop_x_times)
+
+
+  array.pop(2)
+  
+  
+  #-X-
+  # array.pop()
+  # array.pop()
+  
+end
+
+# pop_with_args
+#   takes in an argument of an array and uses the pop method with an argument of 2 to remove the last two array items and return them (FAILED - 6)
+#   decreases the length of the array by 2 (FAILED - 7)
+
+
+
+
+# [V]
+
+def 
+  
+  using_shift(array)
+  #-X-  (arg_of_array) // empalm //
+  
+  array.shift()
+  
+end
+
+# using_shift
+#   takes in an argument of an array and uses the shift method to remove the first item and return it (FAILED - 8)
+#   decreases the length of the array by 1 (FAILED - 9)
+
+
+
+
+#[V]
+
+def 
+  
+  shift_with_args(array)    #XX- arg_of_array)
+  #{...}(shift_x_times)
+
+
+  array.shift(2)
+  
+end
+
+# shift_with_args
+#   takes in an argument of an array and uses the shift method with an argument of 2 to remove and return the first 2 items from the array (FAILED - 10)
+#   decreases the length of the array by 2 (FAILED - 11)
+
